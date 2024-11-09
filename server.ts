77948c2c4062669a6c685586ab5816bee2572430
Matt <vartanian.matt@gmail.com>
2024-11-09 14:46:44 -0500

    typescript for node ws server

---
+++ server.ts
@@ -0,0 +1,39 @@
+import * as WebSocket from 'ws';
+
+class WebSocketServer {
+    private _port: number;
+    private _server: WebSocket.WebSocketServer;
+
+    constructor() {
+        this._port = 5000;
+        this._server = new WebSocket.WebSocketServer({
+            port: this._port
+        });
+    }
+
+    public start = () => {
+        this._server.on("connection", (socket: WebSocket) => {
+            console.log("A client just connected.");
+
+            this._server.on('message', (message: WebSocket.RawData) => {
+                console.log(message);
+
+                this.broadcastMessage(message.toString());
+            });
+        });
+
+        console.log((new Date()) + " Server is listening on port " + this._port);
+    }
+
+    public broadcastMessage = (message: string) => {
+        this._server.clients.forEach((client: WebSocket.WebSocket) => {
+            client.send(message);
+        })
+    };
+}
+
+const server = new WebSocketServer();
+server.start();
+
+
+export { WebSocketServer };
